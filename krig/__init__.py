c015c19ca0a658305dad908b96454825bb5471cc
Ruhong <seahrh@gmail.com>
2020-01-23 22:12:02 +0800

    add function to list all files rooted at given directory

---
+++ krig/__init__.py
@@ -0,0 +1,3 @@
+from .krig import *
+__all__ = []
+__all__ += krig.__all__
---
+++ krig/krig.py
@@ -0,0 +1,14 @@
+__all__ = [
+    'file_paths'
+]
+
+import os
+
+
+def file_paths(root_directory):
+    """Returns a list of file paths rooted at the given directory."""
+    res = []
+    for dirname, _, filenames in os.walk(root_directory):
+        for filename in filenames:
+            res.append(os.path.join(dirname, filename))
+    return res
