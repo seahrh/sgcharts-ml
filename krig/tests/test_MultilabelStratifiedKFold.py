85ba69f2922173afb4f1f0acfbde9efd1fb11cb5
Ruhong <seahrh@gmail.com>
2020-01-28 21:02:04 +0800

    test multi-label stratified kfold: remove proba values

--- krig/tests/test_MultilabelStratifiedKFold.py
+++ krig/tests/test_MultilabelStratifiedKFold.py
@@ -7,14 +7,15 @@ from krig import MultilabelStratifiedKFold
 class TestMultilabelStratifiedKFold:
 
     def test_label_proportion_in_each_fold(self):
+        n_samples = 200
+        labels = [3, 10, 30]
+        folds = [2, 3, 5, 10]
 
         # noinspection PyShadowingNames
-        def _test(n_labels, n_samples, n_splits, is_proba):
+        def _test(n_labels, n_samples, n_splits):
             x = np.zeros((n_samples, 2))  # This is not used in the split
-            if is_proba:
-                y = np.random.uniform(0.0, 1.0000000001, size=(n_samples, n_labels))
-            else:
-                y = np.random.randint(0, 2, size=(n_samples, n_labels))
+            # y = np.random.uniform(0.0, 1.0000000001, size=(n_samples, n_labels))
+            y = np.random.randint(0, 2, size=(n_samples, n_labels))
             mskf = MultilabelStratifiedKFold(n_splits=n_splits)
 
             for train_index, test_index in mskf.split(x, y):
@@ -24,20 +25,8 @@ class TestMultilabelStratifiedKFold:
                 y_test = np.sum(y[test_index], axis=0)
                 y_test = y_test / np.sum(y_test)
                 for r1, r2 in zip(y_train, y_test):
-                    if is_proba:
-                        assert math.fabs(r1 - r2) <= .12
-                    else:
-                        assert math.fabs(r1 - r2) <= .07
+                    assert math.fabs(r1 - r2) < .08
 
-        for is_proba in [False, True]:
-            _test(3, 200, n_splits=3, is_proba=is_proba)
-            _test(10, 200, n_splits=3, is_proba=is_proba)
-            _test(30, 200, n_splits=3, is_proba=is_proba)
-
-            _test(3, 200, n_splits=5, is_proba=is_proba)
-            _test(10, 200, n_splits=5, is_proba=is_proba)
-            _test(30, 200, n_splits=5, is_proba=is_proba)
-
-            _test(3, 200, n_splits=10, is_proba=is_proba)
-            _test(10, 200, n_splits=10, is_proba=is_proba)
-            _test(30, 200, n_splits=10, is_proba=is_proba)
+        for n_labels in labels:
+            for n_splits in folds:
+                _test(n_labels=n_labels, n_samples=n_samples, n_splits=n_splits)
