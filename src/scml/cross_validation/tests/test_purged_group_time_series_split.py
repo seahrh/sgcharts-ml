7d79d3419ab5a813d86459b16b43459d982ddac6
ruhong <ruhong.seah@mann-hummel.com>
2021-01-15 13:01:27 +0800

    test max test group size

--- src/scml/cross_validation/tests/test_purged_group_time_series_split.py
+++ src/scml/cross_validation/tests/test_purged_group_time_series_split.py
@@ -6,7 +6,7 @@ from scml.cross_validation import PurgedGroupTimeSeriesSplit
 
 class TestPurgedGroupTimeSeriesSplit:
     @pytest.fixture(scope="class")
-    def even_group_sizes(self) -> Tuple[np.ndarray, np.ndarray]:
+    def even_sized_groups(self) -> Tuple[np.ndarray, np.ndarray]:
         n_samples = 20
         n_groups = 10
         X = np.random.random(size=(n_samples, 2))
@@ -15,8 +15,15 @@ class TestPurgedGroupTimeSeriesSplit:
         )
         return X, groups
 
-    def test_group_gap_equals_1(self, even_group_sizes):
-        X, groups = even_group_sizes
+    @pytest.fixture(scope="class")
+    def uneven_sized_groups(self) -> Tuple[np.ndarray, np.ndarray]:
+        n_samples = 20
+        X = np.random.random(size=(n_samples, 2))
+        groups = np.array([0, 1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9])
+        return X, groups
+
+    def test_group_gap_equals_1(self, even_sized_groups):
+        X, groups = even_sized_groups
         for fold, (ti, vi) in enumerate(
             PurgedGroupTimeSeriesSplit(
                 n_splits=5,
@@ -52,8 +59,8 @@ class TestPurgedGroupTimeSeriesSplit:
                 assert vi == [18, 19]
                 assert set(groups[vi]) == {9}
 
-    def test_group_gap_equals_2(self, even_group_sizes):
-        X, groups = even_group_sizes
+    def test_group_gap_equals_2(self, even_sized_groups):
+        X, groups = even_sized_groups
         for fold, (ti, vi) in enumerate(
             PurgedGroupTimeSeriesSplit(
                 n_splits=5,
@@ -89,8 +96,8 @@ class TestPurgedGroupTimeSeriesSplit:
                 assert vi == [18, 19]
                 assert set(groups[vi]) == {9}
 
-    def test_max_train_group_size(self, even_group_sizes):
-        X, groups = even_group_sizes
+    def test_max_train_group_size(self, even_sized_groups):
+        X, groups = even_sized_groups
         for fold, (ti, vi) in enumerate(
             PurgedGroupTimeSeriesSplit(
                 n_splits=5,
@@ -125,3 +132,30 @@ class TestPurgedGroupTimeSeriesSplit:
                 assert set(groups[ti]) == {6, 7}
                 assert vi == [18, 19]
                 assert set(groups[vi]) == {9}
+
+    def test_max_test_group_size(self, uneven_sized_groups):
+        X, groups = uneven_sized_groups
+        for fold, (ti, vi) in enumerate(
+            PurgedGroupTimeSeriesSplit(
+                n_splits=3,
+                group_gap=1,
+                max_train_group_size=None,
+                max_test_group_size=2,
+            ).split(X=X, y=None, groups=groups)
+        ):
+            assert fold != 3
+            if fold == 0:
+                assert ti == [0, 1, 2]
+                assert set(groups[ti]) == {0, 1, 2}
+                assert vi == [4, 5, 6, 7]
+                assert set(groups[vi]) == {4, 5}
+            if fold == 1:
+                assert ti == [0, 1, 2, 3, 4, 5]
+                assert set(groups[ti]) == {0, 1, 2, 3, 4}
+                assert vi == [8, 9, 10, 11, 12]
+                assert set(groups[vi]) == {6, 7}
+            if fold == 2:
+                assert ti == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                assert set(groups[ti]) == {0, 1, 2, 3, 4, 5, 6}
+                assert vi == [13, 14, 15, 16, 17, 18, 19]
+                assert set(groups[vi]) == {8, 9}
