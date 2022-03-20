6e5a4a2189356672ba850e440dd0079de3e26342
Ruhong <seahrh@gmail.com>
2022-03-20 17:31:40 +0800

    fix timer test

--- src/scml/tests/test_timex.py
+++ src/scml/tests/test_timex.py
@@ -9,4 +9,4 @@ class TestTimer:
         with Timer() as tim:
             time.sleep(0.5)
         time.sleep(0.5)
-        assert math.isclose(0.5, tim.elapsed.total_seconds(), rel_tol=1e-3)
+        assert math.isclose(0.5, tim.elapsed.total_seconds(), abs_tol=1e-2)
