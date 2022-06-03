60369dc017e8178af5b658c40954ba4c20f4c9a6
Ruhong <seahrh@gmail.com>
2022-06-04 00:41:48 +0800

    Dice distance, similarity

--- src/scml/distances.py
+++ src/scml/distances.py
@@ -1,9 +1,9 @@
 from typing import Set
 
-__all__ = ["jaccard_sim", "jaccard"]
+__all__ = ["jaccard_sim", "jaccard", "dice_sim", "dice"]
 
 
-def jaccard_sim(a: Set[str], b: Set[str]) -> float:
+def jaccard_sim(a: Set, b: Set) -> float:
     """Jaccard similarity as defined in https://en.wikipedia.org/wiki/Jaccard_index"""
     if len(a) == 0 and len(b) == 0:
         return 1
@@ -13,6 +13,21 @@ def jaccard_sim(a: Set[str], b: Set[str]) -> float:
     return len(a & b) / len(un)
 
 
-def jaccard(a: Set[str], b: Set[str]) -> float:
+def jaccard(a: Set, b: Set) -> float:
     """Jaccard distance as defined in https://en.wikipedia.org/wiki/Jaccard_index"""
     return 1 - jaccard_sim(a, b)
+
+
+def dice_sim(a: Set, b: Set) -> float:
+    """Dice coefficient as defined in https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient"""
+    if len(a) == 0 and len(b) == 0:
+        return 1
+    return 2 * len(a & b) / (len(a) + len(b))
+
+
+def dice(a: Set, b: Set) -> float:
+    """Dice distance as defined in https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+
+    Not a proper distance metric as it does not satisfy the triangle inequality.
+    """
+    return 1 - dice_sim(a, b)
--- src/scml/tests/test_distances.py
+++ src/scml/tests/test_distances.py
@@ -1,3 +1,5 @@
+import math
+
 from scml.distances import *
 
 
@@ -17,6 +19,12 @@ class TestJaccard:
         assert jaccard(b, a) == 1
 
     def test_both_sets_nonempty(self):
+        a = {-1}
+        b = {1, 2, 3, 4}
+        assert jaccard_sim(a, b) == 0
+        assert jaccard(a, b) == 1
+        assert jaccard_sim(b, a) == 0
+        assert jaccard(b, a) == 1
         a = {1}
         b = {1, 2, 3, 4}
         assert jaccard_sim(a, b) == 0.25
@@ -41,3 +49,51 @@ class TestJaccard:
         assert jaccard(a, b) == 0
         assert jaccard_sim(b, a) == 1
         assert jaccard(b, a) == 0
+
+
+class TestDice:
+    def test_both_sets_empty(self):
+        a = set()
+        b = set()
+        assert dice_sim(a, b) == 1
+        assert dice(a, b) == 0
+
+    def test_one_empty_set_and_one_nonempty_set(self):
+        a = set()
+        b = {1}
+        assert dice_sim(a, b) == 0
+        assert dice(a, b) == 1
+        assert dice_sim(b, a) == 0
+        assert dice(b, a) == 1
+
+    def test_both_sets_nonempty(self):
+        a = {-1}
+        b = {1, 2, 3, 4}
+        assert dice_sim(a, b) == 0
+        assert dice(a, b) == 1
+        assert dice_sim(b, a) == 0
+        assert dice(b, a) == 1
+        a = {1}
+        b = {1, 2, 3, 4}
+        assert dice_sim(a, b) == 0.4
+        assert dice(a, b) == 0.6
+        assert dice_sim(b, a) == 0.4
+        assert dice(b, a) == 0.6
+        a = {1, 2}
+        b = {1, 2, 3, 4}
+        assert math.isclose(dice_sim(a, b), 2 / 3)
+        assert math.isclose(dice(a, b), 1 / 3)
+        assert math.isclose(dice_sim(b, a), 2 / 3)
+        assert math.isclose(dice(b, a), 1 / 3)
+        a = {1, 2, 3}
+        b = {1, 2, 3, 4}
+        assert math.isclose(dice_sim(a, b), 6 / 7)
+        assert math.isclose(dice(a, b), 1 / 7)
+        assert math.isclose(dice_sim(b, a), 6 / 7)
+        assert math.isclose(dice(b, a), 1 / 7)
+        a = {1, 2, 3, 4}
+        b = {1, 2, 3, 4}
+        assert dice_sim(a, b) == 1
+        assert dice(a, b) == 0
+        assert dice_sim(b, a) == 1
+        assert dice(b, a) == 0
