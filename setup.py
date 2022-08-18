301e916be98ccd4901b96e551ca78073937a65eb
Ruhong <seahrh@gmail.com>
2022-08-18 09:32:04 +0800

    promote torch to required dep

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         "pandas>=1.0.0",
         "scikit-learn>=1.0.2",
         "networkx>=2.6.3",
+        "torch>=1.12.0",
     ],
     extras_require={
         "lint": [
@@ -24,7 +25,6 @@ setup(
             "pytest-cov==3.0.0",
         ],
         "notebook": ["jupyterlab>=1.2.10", "tqdm>=4.45.0"],
-        "torch": ["torch>=1.12.0"],
         "transformers": ["transformers>=4.20.1"],
     },
     packages=find_packages("src", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
