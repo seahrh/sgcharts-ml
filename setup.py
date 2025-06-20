b91da156c60eda314c342077097cbad761e27d60
Ruhong <seahrh@gmail.com>
2025-06-20 10:25:49 +0800

    create embeddings package: communities detection for clustering

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
         "numba>=0.59.0",  # official support for python 3.12 https://github.com/numba/numba/issues/9197
         "pandas>=1.0.0",
         "scikit-learn>=1.3.0",
-        "networkx>=2.6.3",
-        "torch>=2.7.1",
+        "networkx>=3.3",
+        "torch>=2.7.0",
     ],
     extras_require={
         "lint": [
